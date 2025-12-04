4c0ac725c2b0fd4ad797a703a41e3f83c488a2ac
ltmervin123 <ltmervin123@gmail.com>
2025-12-04 08:05:38 +0800

    feat: update useFetchChartData hook to include retry logic and refetch on window focus

--- src/pages/Dashboards/Administrator/Dashboard/chartData.js
+++ src/pages/Dashboards/Administrator/Dashboard/chartData.js
@@ -2,18 +2,17 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { ROLE } from '../../../../../utils/role';
 
-// Custom hook to fetch bar chart data
 export const useFetchChartData = () => {
   return useQuery({
-    queryKey: ['chartData'], // Unique key for caching
+    queryKey: ['chartData'],
     queryFn: async () => {
       const response = await axios.get(
         `${import.meta.env.VITE_API_URL}/${ROLE.ADMINISTRATOR}/chart-data`,
         { withCredentials: true }
       );
-      return response.data; // Return data from backend
+      return response.data;
     },
-    staleTime: 1000 * 60 * 5, // 5 minutes cache duration
-    retry: 2, // Retry twice on failure
+    staleTime: 1000 * 60 * 5,
+    refetchOnWindowFocus: 'always',
   });
 };
