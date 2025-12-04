bb8e5b5a715ed3990b4969d31730b876091c6ce5
alvin <ltmmervin123@gmail.com>
2025-12-04 15:58:39 +0800

    feat: update forms and layouts to conditionally render based on user verification status

--- src/pages/Dashboards/ManpowerProvider/Dashboard/DashboardLayout.jsx
+++ src/pages/Dashboards/ManpowerProvider/Dashboard/DashboardLayout.jsx
@@ -6,80 +6,121 @@ import { ROLE } from '../../../../../utils/role';
 import BrowseJob from './BrowseJob';
 import IndividualJobPostForm from '../../../../components/CreateJobPost/IndividualJobPostForm';
 import TeamJobPostForm from '../../../../components/CreateJobPost/TeamJobPostForm';
+import { useUserProfile } from '../../../../../hooks/useUserProfiles';
+import VerificationStatus from '../../../Dashboards/ManpowerProvider/VerificationForm/VerificationStatus';
+import Form from '../VerificationForm/Form';
+
 const DashboardLayout = () => {
   const [showJobPostModal, setShowJobPostModal] = useState(false);
   const [showFindWorkers, setShowFindWorkers] = useState(false);
   const [postType, setPostType] = useState('');
+  const [showForm, setShowForm] = useState(false);
+  const {
+    data: provider,
+    isLoading: isProviderLoading,
+    isError,
+    error,
+    refetch,
+  } = useUserProfile(ROLE.MANPOWER_PROVIDER);
+
+  const openForm = () => {
+    document.body.style.overflow = 'hidden';
+    setShowForm(true);
+  };
+
+  if (isProviderLoading) return <div className="p-10">Loading...</div>;
 
   return (
     <>
       <Sidebar />
-      <div className="relative min-h-screen bg-linear-to-b from-white to-cyan-400 pl-70 pr-10 pt-30">
-        {/* Header */}
-        <div className="bg-white shadow-md py-6 px-10 mb-8 flex justify-between items-center">
-          <div>
-            <h1 className="text-2xl font-bold text-[#003479]">
-              {showFindWorkers ? 'Find Workers' : 'Welcome, Employer!'}
-            </h1>
-            <p>
-              {showFindWorkers
-                ? 'Browse available workers from manpower agencies'
-                : 'Your hiring platform statistics at a glance'}
-            </p>
+      {provider && provider.is_verified ? (
+        <div className="relative min-h-screen bg-linear-to-b from-white to-cyan-400 pl-70 pr-10 pt-30">
+          {/* Header */}
+          <div className="bg-white shadow-md py-6 px-10 mb-8 flex justify-between items-center">
+            <div>
+              <h1 className="text-2xl font-bold text-[#003479]">
+                {showFindWorkers ? 'Find Workers' : 'Welcome, Employer!'}
+              </h1>
+              <p>
+                {showFindWorkers
+                  ? 'Browse available workers from manpower agencies'
+                  : 'Your hiring platform statistics at a glance'}
+              </p>
+            </div>
+
+            {/* RIGHT SIDE BUTTONS */}
+            <div className="flex gap-3">
+              <button
+                onClick={() => setShowFindWorkers(!showFindWorkers)}
+                className="text-blue-900 border border-blue-900 font-semibold px-10 py-1 rounded hover:bg-blue-700 hover:text-white transition cursor-pointer"
+              >
+                {showFindWorkers ? '← Dashboard' : 'Find Jobs'}
+              </button>
+
+              <select
+                className="text-[#6B7280] outline rounded-lg border-[#6B7280] px-5 py-2  transition cursor-pointer"
+                onChange={(e) => {
+                  const value = e.target.value;
+                  if (!value) return;
+                  setPostType(value);
+                  setShowJobPostModal(true);
+                }}
+              >
+                <option value="" hidden>
+                  {' '}
+                  + Post Job{' '}
+                </option>
+                <option value="hiring">Hiring Post</option>
+                <option value="individual">Individual Post</option>
+                <option value="team">Team Post</option>
+              </select>
+            </div>
           </div>
 
-          {/* RIGHT SIDE BUTTONS */}
-          <div className="flex gap-3">
-            <button
-              onClick={() => setShowFindWorkers(!showFindWorkers)}
-              className="text-blue-900 border border-blue-900 font-semibold px-10 py-1 rounded hover:bg-blue-700 hover:text-white transition cursor-pointer"
-            >
-              {showFindWorkers ? '← Dashboard' : 'Find Jobs'}
-            </button>
-
-            <select
-              className="text-[#6B7280] outline rounded-lg border-[#6B7280] px-5 py-2  transition cursor-pointer"
-              onChange={(e) => {
-                const value = e.target.value;
-                if (!value) return;
-                setPostType(value);
-                setShowJobPostModal(true);
-              }}
-            >
-              <option value="" hidden> + Post Job </option>
-              <option value="hiring">Hiring Post</option>
-              <option value="individual">Individual Post</option>
-              <option value="team">Team Post</option>
-            </select>
-
+          {/* Conditional Content */}
+          {showFindWorkers ? <BrowseJob /> : <MainContent />}
+        </div>
+      ) : (
+        <div className='className="relative min-h-screen bg-linear-to-b from-white to-cyan-400 pl-70 pr-10 pt-30'>
+          <div className="bg-white shadow-md p-6 w-full border border-gray-300 px-20">
+            <VerificationStatus profileData={provider} openForm={openForm} />
           </div>
         </div>
+      )}
 
-        {/* Conditional Content */}
-        {showFindWorkers ? <BrowseJob /> : <MainContent />}
-      </div>
+      {showForm && (
+        <Form
+          onClose={() => {
+            setShowForm(false);
+            document.body.style.overflow = 'auto';
+          }}
+          onSubmitSuccess={() => {
+            setShowForm(false);
+            document.body.style.overflow = 'auto';
+            refetch();
+          }}
+        />
+      )}
 
-       
-        {/* Job Post Modal */}
-        {showJobPostModal && (
-          postType === 'individual' ? (
-            <IndividualJobPostForm
-              onClose={() => setShowJobPostModal(false)}
-              role={ROLE.MANPOWER_PROVIDER}
-            />
-          ) : postType === 'team' ? (
-            <TeamJobPostForm
-              onClose={() => setShowJobPostModal(false)}
-              role={ROLE.MANPOWER_PROVIDER}
-            />
-          ) : (
-            <JobPostForm
-              onClose={() => setShowJobPostModal(false)}
-              role={ROLE.MANPOWER_PROVIDER}
-              postType={postType}
-            />
-          )
-        )}
+      {/* Job Post Modal */}
+      {showJobPostModal &&
+        (postType === 'individual' ? (
+          <IndividualJobPostForm
+            onClose={() => setShowJobPostModal(false)}
+            role={ROLE.MANPOWER_PROVIDER}
+          />
+        ) : postType === 'team' ? (
+          <TeamJobPostForm
+            onClose={() => setShowJobPostModal(false)}
+            role={ROLE.MANPOWER_PROVIDER}
+          />
+        ) : (
+          <JobPostForm
+            onClose={() => setShowJobPostModal(false)}
+            role={ROLE.MANPOWER_PROVIDER}
+            postType={postType}
+          />
+        ))}
     </>
   );
 };
